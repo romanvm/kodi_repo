5fc0aec16edb06ea056cbff96832ec46b9c5804e
Roman Miroshnychenko <romanvm@yandex.ru>
2016-03-27 13:11:42 +0300

    Adds license to @generate.py

--- repo/@generate.py
+++ repo/@generate.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python
+# *
+# *  Copyright (C) 2014-2015 Roman Miroshnychenko
+# *  Copyright (C) 2012-2013 Garrett Brown
+# *  Copyright (C) 2010      j48antialias
+# *
+# *  This Program is free software; you can redistribute it and/or modify
+# *  it under the terms of the GNU General Public License as published by
+# *  the Free Software Foundation; either version 2, or (at your option)
+# *  any later version.
+# *
+# *  This Program is distributed in the hope that it will be useful,
+# *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+# *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# *  GNU General Public License for more details.
+# *
+# *  You should have received a copy of the GNU General Public License
+# *  along with XBMC; see the file COPYING.  If not, write to
+# *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+# *  http://www.gnu.org/copyleft/gpl.html
+# *
+# *  Based on code by j48antialias:
+# *  https://anarchintosh-projects.googlecode.com/files/addons_xml_generator.py
 
 from __future__ import print_function
 import os
